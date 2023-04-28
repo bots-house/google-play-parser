3f8f9d740af5d0d90467ffa8a7cbeaf4e4d82fa8
nikkibuster <dethrock@gmail.com>
2023-04-28 14:03:38 +0300

    feat(cli): small fixes

--- models/app.go
+++ models/app.go
@@ -7,54 +7,54 @@ import (
 )
 
 type App struct {
-	AppID                    string
-	URL                      string
-	Title                    string
-	Description              string
-	DescriptionText          string
-	Summary                  string
-	Installs                 string
-	MinInstalls              float64
-	MaxInstalls              float64
-	Currency                 string
-	Price                    float64
-	PriceText                string
-	Free                     bool
-	Score                    float64
-	ScoreText                string
-	Ratings                  float64
-	Reviews                  float64
-	Histogram                map[int]float64
-	Available                bool
-	OffersIAP                bool
-	IAPRange                 string
-	AndroidVersion           string
-	AndroidVersionText       string
-	Developer                string
-	DeveloperID              string
-	DeveloperEmail           string
-	DeveloperWebsite         string
-	DeveloperAddress         string
-	PrivacyPolicy            string
-	DeveloperInternalID      string
-	Genre                    string
-	GenreID                  string
-	FamilyGenre              string
-	FamilyGenreID            string
-	Icon                     string
-	HeaderImage              string
-	Screenshots              []string
-	Video                    string
-	VideoImage               string
-	PreviewVideo             string
-	ContentRating            string
-	ContentRatingDescription string
-	AdSupported              bool
-	Released                 string
-	Updated                  float64
-	Version                  string
-	RecentChanges            string
-	Comments                 []any
+	AppID                    string          `json:"app_id"`
+	URL                      string          `json:"url"`
+	Title                    string          `json:"title"`
+	Description              string          `json:"description,omitempty"`
+	DescriptionText          string          `json:"description_text,omitempty"`
+	Summary                  string          `json:"summary"`
+	Installs                 string          `json:"installs,omitempty"`
+	MinInstalls              float64         `json:"min_installs,omitempty"`
+	MaxInstalls              float64         `json:"max_installs,omitempty"`
+	Currency                 string          `json:"currency"`
+	Price                    float64         `json:"price,omitempty"`
+	PriceText                string          `json:"price_text,omitempty"`
+	Free                     bool            `json:"free"`
+	Score                    float64         `json:"score"`
+	ScoreText                string          `json:"score_text"`
+	Ratings                  float64         `json:"ratings,omitempty"`
+	Reviews                  float64         `json:"reviews,omitempty"`
+	Histogram                map[int]float64 `json:"histogram,omitempty"`
+	Available                bool            `json:"available,omitempty"`
+	OffersIAP                bool            `json:"offers_iap,omitempty"`
+	IAPRange                 string          `json:"iap_range,omitempty"`
+	AndroidVersion           string          `json:"android_version,omitempty"`
+	AndroidVersionText       string          `json:"android_version_text,omitempty"`
+	Developer                string          `json:"developer"`
+	DeveloperID              string          `json:"developer_id,omitempty"`
+	DeveloperEmail           string          `json:"developer_email,omitempty"`
+	DeveloperWebsite         string          `json:"developer_website,omitempty"`
+	DeveloperAddress         string          `json:"developer_address,omitempty"`
+	PrivacyPolicy            string          `json:"privacy_policy,omitempty"`
+	DeveloperInternalID      string          `json:"developer_internal_id,omitempty"`
+	Genre                    string          `json:"genre,omitempty"`
+	GenreID                  string          `json:"genre_id,omitempty"`
+	FamilyGenre              string          `json:"family_genre,omitempty"`
+	FamilyGenreID            string          `json:"family_genre_id,omitempty"`
+	Icon                     string          `json:"icon"`
+	HeaderImage              string          `json:"header_image,omitempty"`
+	Screenshots              []string        `json:"screenshots,omitempty"`
+	Video                    string          `json:"video,omitempty"`
+	VideoImage               string          `json:"video_image,omitempty"`
+	PreviewVideo             string          `json:"preview_video,omitempty"`
+	ContentRating            string          `json:"content_rating,omitempty"`
+	ContentRatingDescription string          `json:"content_rating_description,omitempty"`
+	AdSupported              bool            `json:"ad_supported,omitempty"`
+	Released                 string          `json:"released,omitempty"`
+	Updated                  float64         `json:"updated,omitempty"`
+	Version                  string          `json:"version,omitempty"`
+	RecentChanges            string          `json:"recent_changes,omitempty"`
+	Comments                 []any           `json:"comments,omitempty"` // todo: no comments there
 }
 
 func (app App) Assign(rhs App) App {
